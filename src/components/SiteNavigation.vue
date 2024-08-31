2a2116a4bdc034510aa3a9b21e7c79ae9d00da81
deividinacio <deivid_@outlook.com>
2024-08-31 17:32:29 -0300

    correcao da api, adicionado elementos ao layout

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -2,14 +2,13 @@
     <header class="sticky top-0 bg-weather-primary shadow-lg">
         <nav class="container flex-col sm:flex-row items-center gap-4 text-white py-6">
             <RouterLink :to="{name: 'home'}">
-                <div class="flex item-center gap-3">
-                    <i class="fa-solid fa-sun text-2xl"></i>
+                <div class="flex item-center gap-3">    
                     <p class="text-2xl">Local do Clima</p>
                 </div>
             </RouterLink>
             <div class="flex gap-3 flex-1 justify-end">
                <!-- <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i> -->
-                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+              <!-- <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i> -->
             </div>
             <!--
             <BaseModal  >
@@ -23,7 +22,6 @@
 
 <script setup>
     import { RouterLink } from 'vue-router';
-    import BaseModal from './BaseModal.vue';
     
   
     /*
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,52 @@
 
 <template>
   <main class="container text-white">
-    <div class="pt-4 mb-8 relative">
+
+    <div class="mt-5 mb-5 ">
       <input type="text" 
       v-model="queryPesquisa"
       @input="getResultadoPesquisa"
       placeholder="Escolha uma cidade" 
       class="py-2 px-1 w-full bg-transparent boder-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"/>
     
-    
     </div>
-    
+    <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 ">
+          <div class="rounded-3xl
+           bg-weather-secondary 
+           py-10  ring-1 ring-inset h-100 relative">
+            <div v-if="apiResultadoPesquisa" class="mx-auto max-w-xs px-8">
+              <div class="text-center ">
+                 <p>
+                   <span> <i class="fa-solid fa-location-dot mr-5"></i></span>
+                   <span class=" text-3xl font-semibold text-white"> {{apiResultadoPesquisa.data[0].city_name}} </span>
+                   
+                 </p>
+                 <p>
+                   <span class="text-xl  text-white">{{apiResultadoPesquisa.data[0].state_code}}</span>
+                   <span class="text-xl  text-white ml-9">{{apiResultadoPesquisa.data[0].country_code}}</span>
+                 </p>
+                  
+                 <p class="mt-8">
+                    <span class="text-8xl font-bold  text-white">{{Math.round(apiResultadoPesquisa.data[0].temp)}}º</span>
+                    <span class="text-6xl">C</span>
+                     <span class="flex gap-3 flex-1 justify-center">
+                      <img class="object-right " v-bind:src= "caminhoIcon + `${apiResultadoPesquisa.data[0].weather.icon}` + '.png'">
+                     </span>
+                  </p>
+                   
+              <p class="mt-6 text-xs leading-5 text-white">
+                <sup class="text-xl">{{ apiResultadoPesquisa.data[0].weather.description }}</sup>
+              </p>
+              <p class="mt-6 text-xs leading-5 text-white">
+                <span>Nascer Sol: </span>
+                <span class="mr-9">{{ apiResultadoPesquisa.data[0].sunrise}}</span>
+                <span>Por do Sol: </span>
+                <span>{{ apiResultadoPesquisa.data[0].sunset }}</span>
+              </p>
+              </div>
+            </div>
+          </div>
+        </div>
   </main>
 </template>
 
@@ -20,23 +56,24 @@
  import{ref} from 'vue';
  import axios from 'axios';
 
- const queryPesquisa = ref("");
+ const queryPesquisa = ref(""); 
  const queryTimeout = ref(null);
- const keyApi = "ca7ac432";
- const apiResultadoPesquisa= ref(null);
+ const keyApi = "5028c11c3dc64c41a27c455513cfa505";
+ const apiResultadoPesquisa = ref(null);
+ const caminhoIcon = "https://cdn.weatherbit.io/static/img/icons/"; 
 
-
-
- const getResultadoPesquisa = () => {
-    clearTimeout(queryTimeout.value);
+const getResultadoPesquisa = () => {
+  clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
-      if (queryPesquisa.value !=="") {
-        const resultado = await axios.get(`https://api.hgbrasil.com/weather?key=${keyApi}&city_name=${queryPesquisa.value},SP`);
-        apiResultadoPesquisa.value = resultado.data.features;
-        console.log(apiResultadoPesquisa.value);  
-        return;
-      }
-      apiResultadoPesquisa.value = null;
-    }, 300);
- };
+    if(queryPesquisa.value !== "" ) {
+      const resultado = await axios.get(`http://api.weatherbit.io/v2.0/current?&city=${queryPesquisa.value}&country=US&key=${keyApi}&lang=pt`
+      );
+      apiResultadoPesquisa.value = resultado.data;
+    //  console.log(apiResultadoPesquisa.value);
+      return ;
+    }
+  apiResultadoPesquisa.value = null;
+  }, 300);
+};
+
 </script>
